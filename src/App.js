dd1707790410ad4aa100d072ada22a14da9ab7be
DedSeK404 <marwenbennaceur5@gmail.com>
2023-01-09 19:10:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      fullName: "Marwen Bennaceur",
+       bio: "Veteren Graphic designer and amateur web developper",
+        imgSrc: "1.png",
+         profession: "Graphic_designer",
+          shows: true
+    }
+    
+  }
+  render() {
+    return (
+      
+      <div className="Container">
+        <img className="img" src={this.state.imgSrc} alt="0" />
+        <p>Full name:</p>
+        <h1>{this.state.fullName}</h1>
+        <p>Bio:</p>
+        <h1>{this.state.bio}</h1>
+        <p>Profession:</p>
+        <h1>{this.state.profession}</h1>
+        <button onClick={() => this.setState({})}>
+          
+        </button>
+      </div> 
+    );
+  }
 }
 
 export default App;
