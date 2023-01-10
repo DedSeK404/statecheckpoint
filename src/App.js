b7989144872d8331a1938c974a4bf5161c12de5b
DedSeK404 <marwenbennaceur5@gmail.com>
2023-01-10 23:08:19 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -16,29 +16,32 @@ class App extends Component {
       shows: true,
       count: 0,
       intervalId: 0
-    }
+    }}
 
     componentDidMount() {
-      const intervalId = setInterval(() => {
-        this.setState(prevState => {
-          return {
-            count: prevState.count + 1,
-          };
-        });
-      }, 1000);
-    }
-  
+     this.setState({ intervalId:setInterval(() => {
+      this.setState(prevState => {
+        return {
+          count: prevState.count + 1,
+        };
+      });
+    }, 1000)})}
+
+
     componentWillUnmount(){
-      clearInterval(intervalId);
+      clearInterval(this.state.intervalId);
     }
-  
-  
-  }
+
+
+
   render() {
     return (
-      <div className="Container">
-        {this.state.shows ? 
+      <>
+      <div  className="Container">
+        </div>
         
+        {this.state.shows ? 
+
           <div className="box">
           <img className="img" src={this.state.imgSrc} alt="0" />
           <p>Full name:</p>
@@ -47,14 +50,15 @@ class App extends Component {
           <h1>{this.state.bio}</h1>
           <p>Profession:</p>
           <h1>{this.state.profession}</h1>
-        
+
           </div>
-         : `${this.state.count}`}
+         : ""}
         <button className="btn" onClick={() => this.setState({ shows: !this.state.shows })}>
           Click me
         </button>
-        <span>Counter : {this.state.count} </span>
-      </div>
+        <h2 className="counter">Counter : {this.state.count} </h2>
+        </>
+      
     );
   }
 }
